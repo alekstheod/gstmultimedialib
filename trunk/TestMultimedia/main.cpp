38f4edb103204da4f2287f25ca280257134c895f
alekstheod@gmail.com <alekstheod@gmail.com@fc000641-3478-88c2-d46d-a224bcb30854>
2011-02-23 17:41:07 +0000

    Refactoring.

--- trunk/TestMultimedia/main.cpp
+++ trunk/TestMultimedia/main.cpp
@@ -4,7 +4,7 @@
  *  Created on: Feb 18, 2011
  *      Author: m1cRo
  */
-#include <Multimedia/FilterGraph/Audio/AudioFilterGraph.h>
+#include <Multimedia/FilterGraph/Video/GLVideoFilterGraph.h>
 #include <iostream>
 
 using namespace multimedia;
@@ -12,11 +12,11 @@ using namespace std;
 int main(int argc, char** argv){
 	try{
 		gst_init (&argc, &argv);
-		AudioFilterGraph graph("file:///windows/D/sample.mp3");
-		graph.Play();
-		sleep(5000);
+
+		//graph.Play();
 	}catch(const GstException& e){
 		cout<<e.what()<<endl;
 	}
+
 	return 0;
 }
