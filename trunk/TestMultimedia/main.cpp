f605db1553659962d6c365cf4eba6c1dbcc82792
alekstheod@gmail.com <alekstheod@gmail.com@fc000641-3478-88c2-d46d-a224bcb30854>
2011-02-23 21:06:37 +0000

    Added XVidVideoFilterGraph.

--- trunk/TestMultimedia/main.cpp
+++ trunk/TestMultimedia/main.cpp
@@ -4,7 +4,7 @@
  *  Created on: Feb 18, 2011
  *      Author: m1cRo
  */
-#include <Multimedia/FilterGraph/Video/XImageVideoFilterGraph.h>
+#include <Multimedia/FilterGraph/Video/XVideoFilterGraph.h>
 #include <iostream>
 
 using namespace multimedia;
@@ -12,7 +12,7 @@ using namespace std;
 int main(int argc, char** argv){
 	try{
 		gst_init (&argc, &argv);
-		XImageVideoFilterGraph graph("file:///windows/D/test.avi");
+		XVideoFilterGraph graph("file:///windows/D/test.avi");
 		graph.Play();
 	}catch(const GstException& e){
 		cout<<e.what()<<endl;
