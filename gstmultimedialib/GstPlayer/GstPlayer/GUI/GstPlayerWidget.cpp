60fc87f968f69e9a4bb88d7ef168d6eeb014f196
Alex <alekstheod@gmail.com>
2012-11-03 18:23:03 +0000

    Refactoring.

--- gstmultimedialib/GstPlayer/GstPlayer/GUI/GstPlayerWidget.cpp
+++ gstmultimedialib/GstPlayer/GstPlayer/GUI/GstPlayerWidget.cpp
@@ -5,42 +5,42 @@
 namespace gstplayer {
 
 GstPlayerWidget::GstPlayerWidget(QWidget *parent) : QGLWidget(parent) {
-	_timer.setInterval(20);
-	connect(&_timer, SIGNAL( timeout() ), this, SLOT(updateScene()));
+    _timer.setInterval(20);
+    connect(&_timer, SIGNAL( timeout() ), this, SLOT(updateScene()));
 }
 
 GstPlayerWidget::~GstPlayerWidget() {
 }
 
 void GstPlayerWidget::initializeGL() {
-	QRect qRect = this->geometry();
-	gl::Device::RECT rect;
-	rect.left = qRect.left();
-	rect.right = qRect.right();
-	rect.top = qRect.top();
-	rect.bottom = qRect.bottom();
-	_glDevice = new gl::Device(rect);
-	_camera = new gl::SimpleCamera;
-	_camera->setPosition(gl::Vertex(0.0f, 0.0f, 2.3f));
-	_glDevice->setCamera(_camera);
-	_menu = new MenuModel;
-	_glDevice->addGLModel(_menu);
-	_track = new VideoTrack( "file:///home/alekstheod/Documents/test.avi", _glDevice);
-	_track->start(QThread::NormalPriority);
-	_timer.start(20);
+    QRect qRect = this->geometry();
+    gl::Device::RECT rect;
+    rect.left = qRect.left();
+    rect.right = qRect.right();
+    rect.top = qRect.top();
+    rect.bottom = qRect.bottom();
+    _glDevice = new gl::Device(rect);
+    _camera = new gl::SimpleCamera;
+    _camera->setPosition(gl::Vertex(0.0f, 0.0f, 2.3f));
+    _glDevice->setCamera(_camera);
+    _menu = new MenuModel;
+    _glDevice->addGLModel(_menu);
+    _track = new VideoTrack( "file:///media/21FE41F33EFE25F8/Multimedia/test.avi", _glDevice);
+    _track->start(QThread::NormalPriority);
+    _timer.start(20);
 }
 
 void GstPlayerWidget::resizeGL(int width, int height) {
-	_glDevice->setPerspective(width, height);
+    _glDevice->setPerspective(width, height);
 }
 
 void GstPlayerWidget::paintGL() {
-	_glDevice->drawModels();
-	this->update();
+    _glDevice->drawModels();
+    this->update();
 }
 
 void GstPlayerWidget::updateScene() {
-	paintGL();
+    paintGL();
 }
 }
 
