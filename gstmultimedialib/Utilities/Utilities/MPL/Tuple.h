b80ecf11a133e151c798acfc555d42301efbd314
Alex <alekstheod@gmail.com>
2014-06-20 05:56:20 +0000

    refactoring for tuple.

--- gstmultimedialib/Utilities/Utilities/MPL/Tuple.h
+++ gstmultimedialib/Utilities/Utilities/MPL/Tuple.h
@@ -1,35 +1,59 @@
-#ifndef TUPLE_H
-#define TYPLE_H
+#ifndef Tuple_h
+#define Tuple_h
 #include <tuple>
 
-namespace std
+namespace utils
 {
 
-namespace Private
+namespace detail
 {
 
 template < uint N >
-struct apply_t {
+struct for_each_t {
     template < typename Functor, typename... ArgsT >
-    static void apply_tuple ( Functor func, std::tuple<ArgsT...>& t ) {
+    static void exec ( std::tuple<ArgsT...>& t, Functor func ) {
         func ( std::get<N-1> ( t ) );
-        apply_t<N-1>::apply_tuple ( func,  t );
+        for_each_t<N-1>::exec ( t, func );
     }
 };
 
 template <>
-struct apply_t<0> {
+struct for_each_t<0> {
     template < typename Functor, typename... ArgsT>
-    static void apply_tuple ( Functor func,  std::tuple<ArgsT...>& t ) {
+    static void exec ( std::tuple<ArgsT...>& t, Functor func ) {
     }
 };
 
+template <class T, std::size_t N, class... Args>
+struct get
+{
+    static constexpr auto value = N;
+};
+
+template <class T, std::size_t N, class... Args>
+struct get<T, N, T, Args...>
+{
+    static constexpr auto value = N;
+};
+
+template <class T, std::size_t N, class U, class... Args>
+struct get<T, N, U, Args...>
+{
+    static constexpr auto value = get<T, N + 1, Args...>::value;
+};
+
+} // namespace detail
+
+template <class T, class... Args>
+T& get(std::tuple<Args...>& t)
+{
+    return std::get<detail::get<T, 0, Args...>::value>(t);
 }
 
 template < typename Functor, typename... ArgsT >
-void for_each ( std::tuple<ArgsT...>& t, Functor func )
+void for_each( std::tuple<ArgsT...>& t, Functor func )
 {
-    Private::apply_t<sizeof... ( ArgsT ) >::apply_tuple ( func, t );
+    detail::for_each_t<sizeof...(ArgsT)>::exec( t, func );
 }
 
 }
