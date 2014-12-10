c1cbeb7fcd2dc354911fe49a905300b41fe54b74
Alex <alekstheod@gmail.com>
2014-12-10 22:52:28 +0000

    Refactoring in progress. 3D model support in progress...

--- GstPlayer/GstPlayer/GUI/GstPlayerWidget.cpp
+++ GstPlayer/GstPlayer/GUI/GstPlayerWidget.cpp
@@ -2,10 +2,14 @@
 #include <GLEngine/Camera/SimpleCamera.h>
 #include <chrono>
 #include <thread>
+#include <boost/units/unit.hpp>
+#include <GLEngine/Rect.h>
+
 
 namespace gstplayer {
 
-GstPlayerWidget::GstPlayerWidget(QWidget *parent) : QGLWidget(parent) {
+GstPlayerWidget::GstPlayerWidget(QWidget *parent) : QGLWidget(parent), 
+						    m_assimp("/home/alekstheod/Downloads/assimp-3.1.1/test/models-nonbsd/3DS/mar_rifle.3ds") {
     m_timer.setInterval(20);
     connect(&m_timer, SIGNAL( timeout() ), this, SLOT(updateScene()));
 }
@@ -15,11 +19,13 @@ GstPlayerWidget::~GstPlayerWidget() {
 
 void GstPlayerWidget::initializeGL() {
     QRect qRect = this->geometry();
-    gl::Scene::RECT<int> rect(qRect.left(), qRect.top(), qRect.right(), qRect.bottom());
+    gl::Rect<int> rect(qRect.left(), qRect.top(), qRect.right(), qRect.bottom());
     m_scene.reset(new gl::Scene(rect, m_camera));
     m_camera.setPosition(gl::Vertex(0.0f, 0.0f, 2.3f));
-    m_scene->add(m_menu);
-    m_track.reset( new VideoTrack( "file:///run/media/alekstheod/D/Multimedia//test.avi", *m_scene) );
+    m_track.reset( new VideoTrack( "file:///run/media/alekstheod/D/Multimedia//test.divx", *m_scene) );;
+    m_assimp.move(gl::Vertex(0.f, 0.f, -95.f));
+    m_scene->add(m_assimp);
+    m_scene->add(m_light);
     m_track->start(QThread::NormalPriority);
     m_timer.start(20);
 }
@@ -30,8 +36,9 @@ void GstPlayerWidget::resizeGL(int width, int height) {
 
 void GstPlayerWidget::paintGL() {
     m_scene->draw();
-    std::chrono::milliseconds dura( 20 );
-    std::this_thread::sleep_for( dura );
+    m_assimp.rotate(gl::Rotation(3.f, 0.f, 2.f, 0.f));
+    std::chrono::milliseconds dur( 20 );
+    std::this_thread::sleep_for( dur );
     this->update();
 }
 
